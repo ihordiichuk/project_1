2985010fbc5eafb5975ae40298db14b00d255ee6
Ihor Hordiichuk <lordwayden@gmail.com>
2025-02-26 22:05:50 +0200

    Code correction
    1. Moved code from wiki urls to encyclopedia urls.
    2. Fixed error with encyclopedia\views. until to util
    3. Fixed error with import in encyclopedia\views

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from markdown2 import Markdown
+import markdown
 
-from . import until
+from . import util
 
 def convert_md_to_html(title):
-    content = until.get_entry(title)
+    content = util.get_entry(title)
     markdowner = markdown.Markdown()
     if content == None:
         return None
